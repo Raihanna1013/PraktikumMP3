c855d6984a923ae8d2abd9fe137ba6cd9d303f1f
Raihanna1013 <55974792+Raihanna1013@users.noreply.github.com>
2021-10-11 19:37:07 +0700

    Add files via upload

---
+++ PraktikumMp2/app/src/main/java/com/example/praktikummp2/Explincit/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.praktikummp2.Explincit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.praktikummp2.R;
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
